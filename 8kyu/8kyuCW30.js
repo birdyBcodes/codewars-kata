fceba0035a5392023b7ac92f6a1a53e819763ccb
Brittany Brooks <brooksoncanvas@gmail.com>
2025-03-04 09:46:25 -0600

    The Wide-Mouthed frog git status clear

---
+++ 8kyu/8kyuCW30.js
@@ -0,0 +1,11 @@
+// Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
+
+//My solution:
+function mouthSize(animal) {
+    animal = animal.toLowerCase()
+    if(animal === 'alligator'){
+      return 'small'
+    }else{
+      return 'wide'
+    }
+  }
